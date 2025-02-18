78dfe6d50f9bc853250d2a6e035e7c0919e11dce
rohyoonseo <ajnaval67@gmail.com>
2025-02-18 23:45:04 +0800

    visual fix

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,7 @@ function App() {
 
   return (
     <>
-      <Index/>
-      <Footer/>
+      <Index />
     </>
   )
 }
--- src/page/Index.jsx
+++ src/page/Index.jsx
@@ -46,89 +46,95 @@ const Index = () => {
   };
 
   return (
-    <div className="min-h-screen w-full mt-10 flex justify-center bg-borat text-white">
-      <div className='max-w-screen-lg w-full p-4'>
-
-        <div className='flex justify-between flex-row'>
-          <div className='mb-5 flex justify-start items-center'>
-            anobakasi'yon
-          </div>
-          <div className='flex mb-5 flex-row text-xs justify-end items-center'>
-            pokingina
-          </div>
-        </div>
-
-        <div className='flex flex-col items-center'>
-          <form onSubmit={handleSubmit} className="relative w-full px-5 items-center">
-            <input
-              onChange={(e) => setWord(e.target.value)}
-              value={word} // Set the input value to match the word state
-              type="text"
-              name="wordInput"
-              placeholder="Search..."
-              className="w-full pl-10 pr-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500"
-            />
-            <span className="absolute left-8 top-[22px] transform -translate-y-1/2 text-gray-400">
-              <FontAwesomeIcon icon={faSearch} />
-            </span>
-          </form>
-        </div>
-
-        {/* Conditional Rendering */}
-        {isLoading && <p className='text-center mt-2'>Loading rahhhhhhhhhhhhhh</p>}
-        {error && <p className="text-red-500 text-center mt-2">Error: {error}</p>}
-        {data && data.length > 0 && (
-          <>
-            <div className='flex flex-row justify-between mt-5'>
-              <div>
-                <h2 className='text-5xl'>{data[0].word}</h2>
-                {data[0].phonetics && data[0].phonetics.length > 0 && (
-                  <p className='text-purple-400'>{data[0].phonetics[0].text}</p>
-                )}
-              </div>
-              <div>
-                {data[0].phonetics && data[0].phonetics.length > 0 && (
-                  <button
-                    className='bg-purple-400 w-12 h-12 rounded-full'
-                    onClick={() => handleAudioPlay(data[0].phonetics[0].audio)}
-                  >
-                    <FontAwesomeIcon icon={faPlay} />
-                  </button>
-                )}
-              </div>
+    <div className="min-h-screen w-full flex flex-col bg-borat text-white">
+      <div className="flex-grow">
+        <div className="mt-10 flex justify-center">
+          <div className="max-w-screen-lg w-full p-4">
+            <div className="flex justify-between flex-row">
+              <div className="mb-5 flex justify-start items-center">verba.</div>
+              <div className="flex mb-5 flex-row text-xs justify-end items-center">108.4.12.6.1</div>
             </div>
 
-            {/* Meanings and Definitions */}
-            {data[0].meanings.map((meaning, index) => (
-              <div key={index} className='mt-10'>
-                <h2 className='text-2xl'>{meaning.partOfSpeech}</h2>
-                <p className='text-gray-500'>Meaning</p>
-                <ul className='list-disc list-outside pl-5'>
-                  {meaning.definitions.map((definition, i) => (
-                    <li key={i} className="mb-2">
-                      {definition.definition}
-                      {definition.example && <p className="italic">Example: {definition.example}</p>}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </>
-        )}
+            <div className="flex flex-col items-center">
+              <form onSubmit={handleSubmit} className="relative w-full px-5 items-center">
+                <input
+                  onChange={(e) => setWord(e.target.value)}
+                  value={word}
+                  type="text"
+                  name="wordInput"
+                  placeholder="Search..."
+                  className="w-full pl-10 pr-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500"
+                />
+                <span className="absolute left-8 top-[22px] transform -translate-y-1/2 text-gray-400">
+                  <FontAwesomeIcon icon={faSearch} />
+                </span>
+              </form>
+            </div>
 
-        {data && data.length > 0 && (
-          <div className='flex flex-row gap-2 mt-20 text-xs'>
-            <p>Source</p>
-            <a
-              href={data[0].sourceUrls}
-              target='_blank'
-              rel='noopener noreferrer'
-              className='underline'
-            >
-              {data[0].sourceUrls}
-            </a>
+            {/* Conditional Rendering */}
+            {isLoading && <p className="text-center mt-2">Loading rahhhhhhhhhhhhhh</p>}
+            {error && <p className="text-red-500 text-center mt-2">Error: {error}</p>}
+            {data && data.length > 0 && (
+              <>
+                <div className="flex flex-row justify-between mt-5">
+                  <div>
+                    <h2 className="text-5xl">{data[0].word}</h2>
+                    {data[0].phonetics && data[0].phonetics.length > 0 && (
+                      <p className="text-purple-400">{data[0].phonetics[0].text}</p>
+                    )}
+                  </div>
+                  <div>
+                    {data[0].phonetics && data[0].phonetics.length > 0 && (
+                      <button
+                        className="bg-purple-400 w-12 h-12 rounded-full"
+                        onClick={() => handleAudioPlay(data[0].phonetics[0].audio)}
+                      >
+                        <FontAwesomeIcon icon={faPlay} />
+                      </button>
+                    )}
+                  </div>
+                </div>
+
+                {/* Meanings and Definitions */}
+                {data[0].meanings.map((meaning, index) => (
+                  <div key={index} className="mt-10">
+                    <h2 className="text-2xl">{meaning.partOfSpeech}</h2>
+                    <p className="text-gray-500">Meaning</p>
+                    <ul className="list-disc list-outside pl-5">
+                      {meaning.definitions.map((definition, i) => (
+                        <li key={i} className="mb-2">
+                          {definition.definition}
+                          {definition.example && (
+                            <p className="italic">Example: {definition.example}</p>
+                          )}
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </>
+            )}
+
+            {data && data.length > 0 && (
+              <div className="flex flex-row gap-2 mt-20 text-xs">
+                <p>Source</p>
+                <a
+                  href={data[0].sourceUrls}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline"
+                >
+                  {data[0].sourceUrls}
+                </a>
+              </div>
+            )}
           </div>
-        )}
+        </div>
+      </div>
+
+      {/* Footer */}
+      <div className='bg-borat text-white flex items-center text-center justify-center p-2 text-xs'>
+        developed by <a href="https://www.shlynav.me/" className='underline ml-1' target='_blank'>shlynav.tiff</a>
       </div>
     </div>
   );
